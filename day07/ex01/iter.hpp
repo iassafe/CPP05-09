f48ef1be189e6c971258d6acf2fa263af9e23f2d
iassafe <ikrameassafe17@gmail.com>
2024-02-19 17:40:29 +0100

    ex01

---
+++ day07/ex01/iter.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   iter.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 17:01:34 by iassafe           #+#    #+#             */
+/*   Updated: 2024/02/19 17:37:39 by iassafe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITER_HPP
+#define ITER_HPP
+#include<iostream>
+
+template<typename T, typename F>
+void iter(T* array, size_t length, F func){
+    if (!array || !length)
+        return ;
+    for(size_t i = 0; i < length; i++){
+        func(array[i]);
+    }
+}
+
+template<typename T>
+void _print(const T& element){
+    std::cout << element << ", ";
+}
+#endif
---
+++ day07/ex01/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 17:01:37 by iassafe           #+#    #+#             */
+/*   Updated: 2024/02/19 17:38:10 by iassafe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"iter.hpp"
+
+int main(){
+    int intArray[] = {1, 2, 3, 4, 5};
+    std::cout << "Integer array: ";
+    ::iter<int>(intArray, 5, _print<int>);
+    std::cout << std::endl;
+
+    std::cout << "-------------" << std::endl;
+    
+    double doubleArray[] = {1.1, 2.2, 3.3, 4.4, 5.5};
+    std::cout << "Double array: ";
+    ::iter<double>(doubleArray, 5, _print<double>);
+    std::cout << std::endl;
+
+    std::cout << "-------------" << std::endl;
+    std::string stringArray[] = {"apple", "banana", "cherry"};
+    std::cout << "String array: ";
+    ::iter<std::string>(stringArray, 3, _print<std::string>);
+    std::cout << std::endl;
+
+    return 0;
+}
