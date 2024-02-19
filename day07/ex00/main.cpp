8989eabdb2a8efbaf98ebcb23204f44a64b6e982
iassafe <ikrameassafe17@gmail.com>
2024-02-19 16:59:36 +0100

    finish ex00 in day07

--- day07/ex00/main.cpp
+++ day07/ex00/main.cpp
@@ -6,7 +6,24 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/14 10:29:33 by iassafe           #+#    #+#             */
-/*   Updated: 2024/02/14 10:29:34 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/02/19 16:58:22 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
+
+int main( void ) {
+    int a = 0;
+    int b = 0;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+    std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+    std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
+    return 0;
+}
---
+++ day07/ex00/whatever.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 16:31:42 by iassafe           #+#    #+#             */
+/*   Updated: 2024/02/19 16:58:53 by iassafe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+#include<iostream>
+
+template <typename T>
+void swap(T& a, T& b){
+    T tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(const T& a, const T& b){
+    return (b <= a) ? b : a;
+}
+
+template <typename T>
+T max(const T& a, const T& b){
+    return (a <= b) ? b : a;
+}
+#endif 
