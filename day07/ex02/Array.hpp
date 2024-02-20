bdc2b9f75e2ffb005f8b69ef37131368128e4240
iassafe <ikrameassafe17@gmail.com>
2024-02-20 16:44:04 +0100

    ex02

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:43:58 by iassafe           #+#    #+#             */
-/*   Updated: 2024/02/20 16:09:34 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/02/20 16:40:50 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,55 +17,60 @@
 #include <iostream>
 #include <exception>
 
+class Out_Of_Range: public std::exception{
+	public:
+		const char* what() const throw();
+};
+
 template <typename T>
-class Array {
-private:
-    T* _data;
-    unsigned int _size;
-
-public:
-    Array() : _data(NULL), _size(0){}
+class Array{
+    private:
+        T* _data;
+        unsigned int _size;
     
-    Array(unsigned int n) : _size(n){
-        _data = new T[_size];
-        for (unsigned int i = 0; i < _size; i++){
-            _data[i] = T();
+    public:
+        Array() : _data(NULL), _size(0){}
+        
+        Array(unsigned int n) : _size(n){
+            _data = new T[_size];
+            for (unsigned int i = 0; i < _size; i++){
+                _data[i] = T();
+            }
         }
-    }
-
-    Array(const Array& other) : _size(other._size){
-        _data = new T[_size];
-        for (unsigned int i = 0; i < _size; i++){
-            _data[i] = other._data[i];
-        }
-    }
-
-    Array& operator=(const Array& other) {
-        if (this != &other){
-            delete[] _data;
-            _size = other._size;
+    
+        Array(const Array& other) : _size(other._size){
             _data = new T[_size];
             for (unsigned int i = 0; i < _size; i++){
                 _data[i] = other._data[i];
             }
         }
-        return *this;
-    }
-
-    ~Array(){
-        delete[] _data;
-    }
-
-    T& operator[](unsigned int index){
-        if (index >= _size){
-            throw ("Index out of bounds");
-        }
-        return _data[index];
-    }
     
-    unsigned int size() const{
-        return _size;
-    }
+        Array& operator=(const Array& other) {
+            if (this != &other){
+                delete[] _data;
+                _size = other._size;
+                _data = new T[_size];
+                for (unsigned int i = 0; i < _size; i++){
+                    _data[i] = other._data[i];
+                }
+            }
+            return *this;
+        }
+    
+        ~Array(){
+            delete[] _data;
+        }
+    
+        T& operator[](unsigned int index){
+            if (index >= _size){
+                throw Out_Of_Range();
+            }
+            return _data[index];
+        }
+        
+        unsigned int size() const{
+            return _size;
+        }
 };
 
 
--- day07/ex02/main.cpp
+++ day07/ex02/main.cpp
@@ -6,17 +6,21 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 17:44:01 by iassafe           #+#    #+#             */
-/*   Updated: 2024/02/20 16:21:04 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/02/20 16:42:34 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"Array.hpp"
 
+const char* Out_Of_Range::what() const throw(){
+    return ("Index out of bounds");
+}
+
 int main(){
     try{
         Array<int> intArray(5);
-        for (unsigned int i = 0; i < intArray.size(); i++){
-            intArray[i] = i;
+        for(unsigned int i = 0; i < intArray.size(); i++){
+            intArray[i] = i + 1;
         }
     
         Array<int> intArrayCopy(intArray);
@@ -24,19 +28,19 @@ int main(){
         intArray[2] = 100;
     
         std::cout << "Original int_Array: ";
-        for (unsigned int i = 0; i < intArray.size(); i++){
+        for(unsigned int i = 0; i < intArray.size(); i++){
             std::cout << intArray[i] << " ";
         }
         std::cout << std::endl;
     
         std::cout << "Copied int_Array: ";
-        for (unsigned int i = 0; i < intArrayCopy.size(); i++){
+        for(unsigned int i = 0; i < intArrayCopy.size(); i++){
             std::cout << intArrayCopy[i] << " ";
         }
         std::cout << std::endl;
         
         Array<std::string> stringArray(2);
-        for (unsigned int i = 0; i < stringArray.size(); i++){
+        for(unsigned int i = 0; i < stringArray.size(); i++){
             stringArray[i] = "one";
         }
     
@@ -45,20 +49,20 @@ int main(){
         stringArray[1] = "two";
     
         std::cout << "Original string_Array: ";
-        for (unsigned int i = 0; i < stringArray.size(); i++){
+        for(unsigned int i = 0; i < stringArray.size(); i++){
             std::cout << stringArray[i] << " ";
         }
         std::cout << std::endl;
 
         stringArrayCopy[0] = "two";
         std::cout << "Copied string_Array: ";
-        for (unsigned int i = 0; i < stringArrayCopy.size(); i++){
+        for(unsigned int i = 0; i < stringArrayCopy.size(); i++){
             std::cout << stringArrayCopy[i] << " ";
         }
         std::cout << std::endl;
     }
-    catch(const char *s){
-        std::cout << "Error: " << s << std::endl;
+    catch(std::exception& e){
+        std::cout << "Error: " << e.what() << std::endl;
     }
     return 0;
 }
