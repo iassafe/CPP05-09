a2d5bb8d261ecff8d88e355e28f4e9c1d050abb5
iassafe <ikrameassafe17@gmail.com>
2024-02-29 16:33:45 +0100

    ex01

--- day09/ex01/RPN.cpp
+++ day09/ex01/RPN.cpp
@@ -6,28 +6,44 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 10:05:22 by iassafe           #+#    #+#             */
-/*   Updated: 2024/02/29 12:53:33 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/02/29 16:33:27 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"RPN.hpp"
 
-
-
 void _split(std::string str){
     std::istringstream iss(str);
     std::string token;
     char *endptr;
+    std::stack<int> stack;
+    int count_nb = 0;
+    int count_op = 0;
     while (iss >> token){
         if (isdigit(token[0])){
+            count_nb++;
             double number = std::strtod(token.c_str(), &endptr);
-            std::cout << "Number: " << number << std::endl;
+            stack.push(number);
         }
-        else if (token == "+" || token == "-" || token == "*" || token == "/"){
-            std::cout << "Operator: " << token << std::endl;
+        else if (stack.size() > 1){
+            count_op++;
+            int nb1 = stack.top();
+            stack.pop();
+            int nb2 = stack.top();
+            stack.pop();
+            if (token == "+")
+                stack.push(nb2 + nb1);
+            else if(token == "-")
+                stack.push(nb2 - nb1);
+            else if(token == "*")
+                stack.push(nb2 * nb1);
+            else if(token == "/")
+                stack.push(nb2 / nb1);
         }
     }
-    
+    if (count_nb != (count_op + 1))
+        throw("Invalid input!");
+    std::cout << stack.top() << std::endl;
 }
 
 void _check(std::string str){
--- day09/ex01/RPN.hpp
+++ day09/ex01/RPN.hpp
@@ -6,15 +6,15 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 10:05:26 by iassafe           #+#    #+#             */
-/*   Updated: 2024/02/29 12:50:37 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/02/29 16:08:14 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RPN_HPP
 #define RPN_HPP
-#include<iostream>
+#include <iostream>
 #include <sstream>
-
+#include <stack>
 
 void _check(std::string str);
 void _split(std::string str);
