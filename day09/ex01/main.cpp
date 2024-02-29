34abc51a3791abddb6de6f3d936c6ad11bd326e3
iassafe <ikrameassafe17@gmail.com>
2024-02-29 11:38:09 +0100

    ex01

---
+++ day09/ex01/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 11:36:27 by iassafe           #+#    #+#             */
+/*   Updated: 2024/02/29 11:36:58 by iassafe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"RPN.hpp"
+
+int main(int ac, char **av){
+    try{
+        if (ac == 2 && av[1][0] != '\0'){
+            std::string str = av[1];
+            _check(str);
+        }
+        else
+            throw("Invalid param!");
+    }
+    catch(const char *s){
+        std::cerr << "Error: " << s << std::endl;
+    }
+}
