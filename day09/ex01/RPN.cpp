5632be806fd9ac13dedd348a33b0291c833d91dc
iassafe <ikrameassafe17@gmail.com>
2024-02-29 12:45:46 +0100

    ex01

--- day09/ex01/RPN.cpp
+++ day09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 10:05:22 by iassafe           #+#    #+#             */
-/*   Updated: 2024/02/29 11:25:21 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/02/29 12:44:16 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,42 @@
 
 void _check(std::string str){
     for(size_t i = 0; i < str.length(); i++){
-        if (!isdigit(str[i]) && str[i] != ' ' && str[i] != '+' \
-            && str[i] != '-' && str[i] != '/' && str[i] != '*')
-            throw("invalid param!");
+        if (str.length() > 2 ){
+            if (!isdigit(str[0]))
+                throw("Invalid input!");
+            else{
+                int k = 1;
+                while(str[k] == ' '){
+                    k++;
+                }
+                if(!isdigit(str[k]))
+                    throw("Invalid input!");
+            }
+        }
+        else if (!isdigit(str[i]) && str[i] != ' ' && \
+            str[i] != '+' && str[i] != '-' && str[i] != '/' && str[i] != '*')
+            throw("Invalid input!");
+        else if ((str[i] == '-' || str[i] == '+') && (str[i + 1] != ' ' && str[i + 1]))
+        {
+            std::cout << str[i] << str[i + 1] << std::endl;
+            throw("Invalid input!");
+        }
+        else if (isdigit(str[i]) && (str[i + 1] != ' ' || ( i > 0 && str[i + 1] != ' ')))
+            throw("Invalid input!");
+        else if (((str[i] == '/' || str[i] == '*') && (str[i + 1] != ' ' && str[i + 1]))
+                || ((str[i] == '/' || str[i] == '*') && str[i - 1] != ' '))
+                throw("Invalid input!");
+    }
+    std::istringstream iss(str);
+    std::string token;
+    char *endptr;
+    while (iss >> token){
+        if (isdigit(token[0])){
+            double number = std::strtod(token.c_str(), &endptr);
+            std::cout << "Number: " << number << std::endl;
+        }
+        else if (token == "+" || token == "-" || token == "*" || token == "/"){
+            std::cout << "Operator: " << token << std::endl;
+        }
     }
 }
--- day09/ex01/RPN.hpp
+++ day09/ex01/RPN.hpp
@@ -6,13 +6,14 @@
 /*   By: iassafe <iassafe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 10:05:26 by iassafe           #+#    #+#             */
-/*   Updated: 2024/02/29 11:30:10 by iassafe          ###   ########.fr       */
+/*   Updated: 2024/02/29 11:47:27 by iassafe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef RPN_HPP
 #define RPN_HPP
 #include<iostream>
+#include <sstream>
 
 
 void _check(std::string str);
